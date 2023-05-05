ee56d82339f9b0f4040018843cd4b99f9e0b9048
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2023-05-05 23:59:28 +0200

    added excercises to k2

---
+++ Excercises/Kolokvium2/kol2_13.cpp
@@ -0,0 +1,423 @@
+// 2 - 13
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Image {
+protected:
+    char* name;
+    char author[50];
+    int width;
+    int height;
+
+private:
+    void copy(const Image& other) {
+        this->name = new char[strlen(other.name) + 1];
+        strcpy(this->name, other.name);
+        strcpy(this->author, other.author);
+        this->width = other.width;
+        this->height = other.height;
+    }
+
+public:
+    Image() {
+        this->name = new char[10];
+        strcpy(this->name, "untitled");
+        strcpy(this->author, "unknown");
+        this->width = this->height = 800;
+    }
+
+    Image(char* name) {
+        this->name = new char[strlen(name) + 1];
+        strcpy(this->name, name);
+        strcpy(this->author, "unknown");
+        this->width = this->height = 800;
+    }
+
+    Image(char* name, char* author) {
+        this->name = new char[strlen(name) + 1];
+        strcpy(this->name, name);
+        strcpy(this->author, author);
+        this->width = this->height = 800;
+    }
+
+    Image(char* name, char* author, int width, int height) {
+        this->name = new char[strlen(name) + 1];
+        strcpy(this->name, name);
+        strcpy(this->author, author);
+        this->width = width;
+        this->height = height;
+    }
+
+    Image(const Image& other) {
+        copy(other);
+    }
+
+    Image& operator=(const Image& other) {
+        if(this == &other) return *this;
+        delete [] name;
+        copy(other);
+        return *this;
+    }
+
+    friend ostream& operator<<(ostream& out, Image& img) {
+        out << img.name << " " << img.author << " " << img.fileSize() << "\n";
+        return out;
+    }
+
+    bool operator>(Image& other) {
+        return fileSize() > other.fileSize();
+    }
+
+    virtual int fileSize() { return width * height * 3; }
+
+    int getPixels() { return width * height; }
+
+    char* getName() { return name; }
+    void setName(char* name) { this->name = new char[strlen(name + 1)]; strcpy(this->name, name);}
+
+    char* getAuthor() { return author; }
+    void setAuthor(char* author) { strcpy(this->author, author); }
+
+    int getWidth() { return width; }
+    void setWidth(int width) { this->width = width;}
+
+    int getHeight() { return height; }
+    void setHeight(int height) { this->height = height; }
+
+    ~Image() {
+        delete [] name;
+    }
+};
+
+class TransparentImage : public Image {
+    bool transparent;
+
+public:
+    TransparentImage() : Image() {
+        this->transparent = true;
+    }
+
+    TransparentImage(char* name, char* author, int width, int height, bool transparent) : Image(name, author, width, height) {
+        this->transparent = transparent;
+    }
+
+    TransparentImage(const TransparentImage& other) : Image(other) {
+        this->transparent = other.transparent;
+    }
+
+    TransparentImage& operator=(const TransparentImage& other) {
+        if(this == &other) return *this;
+        Image::operator=(other);
+        this->transparent = other.transparent;
+        return *this;
+    }
+
+    bool operator>(TransparentImage& other) {
+        return fileSize() > other.fileSize();
+    }
+
+    int fileSize() { return transparent ? width * height * 4 : width * height + (width * height) / 8.0; }
+
+    ~TransparentImage() { }
+};
+
+class Folder {
+    char name[255];
+    char author[50];
+    Image* images[100];
+    int n;
+
+    void copy(const Folder& other) {
+        strcpy(this->name, other.name);
+        strcpy(this->author, other.author);
+        for(int i = 0; i < other.n; i++) this->images[i] = other.images[i];
+        this->n = other.n;
+    }
+
+public:
+    Folder() {
+        strcpy(this->name, "unknown");
+        strcpy(this->author, "unknown");
+        this->n = 0;
+    }
+
+    Folder(char* name, char* author) {
+        strcpy(this->name, name);
+        strcpy(this->author, author);
+        this->n = 0;
+    }
+
+    Folder(const Folder& other) {
+        copy(other);
+    }
+
+    Folder& operator=(const Folder& other) {
+        if(this == &other) return *this;
+        copy(other);
+        return *this;
+    }
+
+    Folder& operator+=(Image& img) {
+        if(n >= 100) return *this;
+        images[n++] = &img;
+        return *this;
+    }
+
+    Image* operator[](int idx) {
+        return idx < n ? images[idx] : NULL;
+    }
+
+    friend ostream& operator<<(ostream& out, Folder& f) {
+        out << f.name << " " << f.author << "\n--\n";
+        for(int i = 0; i < f.n; i++) out << *f.images[i];
+        out << "--\nFolder size: " << f.folderSize() << "\n";
+        return out;
+    }
+
+    int folderSize() {
+        int s = 0;
+        for(int i = 0; i < n; i++) {
+            s += images[i]->fileSize();
+        }
+        return s;
+    }
+
+    Image* getMaxFile() {
+        int idx = -1;
+
+        for(int i = 0; i < n; i++) {
+            if(idx == -1) { idx = i; continue; }
+            if(images[i]->fileSize() > images[idx]->fileSize()) idx = i;
+        }
+
+        if(idx == -1) return NULL;
+
+        return images[idx];
+    }
+
+    ~Folder() { }
+};
+
+Folder& max_folder_size(Folder* f, int n) {
+    int idx = -1;
+
+    for(int i = 0; i < n; i++) {
+        if(idx == -1) { idx = i; continue; }
+        if(f[i].folderSize() > f[idx].folderSize()) idx = i;
+    }
+
+    return f[idx];
+}
+
+int main() {
+    int tc; // Test Case
+
+    char name[255];
+    char user_name[51];
+    int w, h;
+    bool tl;
+
+    cin >> tc;
+
+    if (tc==1){
+        // Testing constructor(s) & operator << for class File
+
+        cin >> name;
+        cin >> user_name;
+        cin >> w;
+        cin >> h;
+
+
+        Image f1;
+
+        cout<< f1;
+
+        Image f2(name);
+        cout<< f2;
+
+        Image f3(name, user_name);
+        cout<< f3;
+
+        Image f4(name, user_name, w, h);
+        cout<< f4;
+    }
+    else if (tc==2){
+        // Testing constructor(s) & operator << for class TextFile
+        cin >> name;
+        cin >> user_name;
+        cin >> w >> h;
+        cin >> tl;
+
+        TransparentImage tf1;
+        cout<< tf1;
+
+        TransparentImage tf4(name, user_name, w, h, tl);
+        cout<< tf4;
+    }
+    else if (tc==3){
+        // Testing constructor(s) & operator << for class Folder
+        cin >> name;
+        cin >> user_name;
+
+        Folder f3(name, user_name);
+        cout<< f3;
+    }
+    else if (tc==4){
+        // Adding files to folder
+        cin >> name;
+        cin >> user_name;
+
+        Folder dir(name, user_name);
+
+        Image * f;
+        TransparentImage *tf;
+
+        int sub, fileType;
+
+        while (1){
+            cin >> sub; // Should we add subfiles to this folder
+            if (!sub) break;
+
+            cin >>fileType;
+            if (fileType == 1){ // Reading File
+                cin >> name;
+                cin >> user_name;
+                cin >> w >> h;
+                f = new Image(name,user_name, w, h);
+                dir += *f;
+            }
+            else if (fileType == 2){
+                cin >> name;
+                cin >> user_name;
+                cin >> w >> h;
+                cin >> tl;
+                tf = new TransparentImage(name,user_name, w, h, tl);
+                dir += *tf;
+            }
+        }
+        cout<<dir;
+    }
+    else if(tc==5){
+        // Testing getMaxFile for folder
+
+        cin >> name;
+        cin >> user_name;
+
+        Folder dir(name, user_name);
+
+        Image* f;
+        TransparentImage* tf;
+
+        int sub, fileType;
+
+        while (1){
+            cin >> sub; // Should we add subfiles to this folder
+            if (!sub) break;
+
+            cin >>fileType;
+            if (fileType == 1){ // Reading File
+                cin >> name;
+                cin >> user_name;
+                cin >> w >> h;
+                f = new Image(name,user_name, w, h);
+                dir += *f;
+            }
+            else if (fileType == 2){
+                cin >> name;
+                cin >> user_name;
+                cin >> w >> h;
+                cin >> tl;
+                tf = new TransparentImage(name,user_name, w, h, tl);
+                dir += *tf;
+            }
+        }
+        cout<< *(dir.getMaxFile());
+    }
+    else if(tc==6){
+        // Testing operator [] for folder
+
+        cin >> name;
+        cin >> user_name;
+
+        Folder dir(name, user_name);
+
+        Image* f;
+        TransparentImage* tf;
+
+        int sub, fileType;
+
+        while (1){
+            cin >> sub; // Should we add subfiles to this folder
+            if (!sub) break;
+
+            cin >>fileType;
+            if (fileType == 1){ // Reading File
+                cin >> name;
+                cin >> user_name;
+                cin >> w >> h;
+                f = new Image(name,user_name, w, h);
+                dir += *f;
+            }
+            else if (fileType == 2){
+                cin >> name;
+                cin >> user_name;
+                cin >> w >> h;
+                cin >> tl;
+                tf = new TransparentImage(name,user_name, w, h, tl);
+                dir += *tf;
+            }
+        }
+
+        cin >> sub; // Reading index of specific file
+        cout<< *dir[sub];
+    }
+    else if(tc==7){
+        // Testing function max_folder_size
+        int folders_num;
+
+        Folder dir_list[10];
+
+        Folder dir;
+        cin >> folders_num;
+
+        for (int i=0; i<folders_num; ++i){
+            cin >> name;
+            cin >> user_name;
+            dir = Folder(name, user_name);
+
+
+            Image* f;
+            TransparentImage *tf;
+
+            int sub, fileType;
+
+            while (1){
+                cin >> sub; // Should we add subfiles to this folder
+                if (!sub) break;
+
+                cin >>fileType;
+                if (fileType == 1){ // Reading File
+                    cin >> name;
+                    cin >> user_name;
+                    cin >> w >> h;
+                    f = new Image(name,user_name, w, h);
+                    dir += *f;
+                }
+                else if (fileType == 2){
+                    cin >> name;
+                    cin >> user_name;
+                    cin >> w >> h;
+                    cin >> tl;
+                    tf = new TransparentImage(name,user_name, w, h, tl);
+                    dir += *tf;
+                }
+            }
+            dir_list[i] = dir;
+        }
+
+        cout<<max_folder_size(dir_list, folders_num);
+    }
+    return 0;
+};
---
+++ Excercises/Kolokvium2/kol2_14.cpp
@@ -0,0 +1,243 @@
+// 2 - 14
+
+#include <iostream>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+class SMS {
+protected:
+    char broj[16];
+    float price;
+    static float ddv;
+
+public:
+    SMS() {
+        strcpy(this->broj, "0038970000000");
+        this->price = 0;
+    }
+
+    SMS(char* broj, float price) {
+        strcpy(this->broj, broj);
+        this->price = price;
+    }
+
+    SMS(const SMS& other) {
+        strcpy(this->broj, other.broj);
+        this->price = other.price;
+    }
+
+    SMS& operator=(const SMS& other) {
+        if(this == &other) return *this;
+        strcpy(this->broj, other.broj);
+        this->price = other.price;
+        return *this;
+    }
+
+    friend ostream& operator<<(ostream& out, SMS& sms) {
+        out << "Tel: " << sms.broj << " - cena: " << sms.SMS_cena() << "den.\n";
+        return out;
+    }
+
+    static void setDanok(float val) { SMS::ddv = val / 100.0; }
+
+    virtual float SMS_cena() = 0;
+
+    ~SMS() { }
+};
+
+class RegularSMS : public SMS {
+    char message[1000];
+    bool roaming;
+    static float mult;
+
+public:
+    RegularSMS() : SMS() {
+        strcpy(this->message, "msg");
+        this->roaming = false;
+    }
+
+    RegularSMS(char* name, float price, char* message, bool roaming) : SMS(name, price) {
+        strcpy(this->message, message);
+        this->roaming = roaming;
+    }
+
+    float SMS_cena() {
+        float x = roaming ? mult : ddv;
+        return price * (1.0 + x) * ceil(strlen(message) / 160.0);
+    }
+
+    static void set_rProcent(float val) { RegularSMS::mult = val / 100; }
+
+    ~RegularSMS() { }
+};
+
+class SpecialSMS : public SMS {
+    bool humane;
+    static float mult;
+
+public:
+    SpecialSMS() : SMS() {
+        this->humane = false;
+    }
+
+    SpecialSMS(char* name, float price, bool humane) : SMS(name, price) {
+        this->humane = humane;
+    }
+
+    static void set_sProcent(float val) { SpecialSMS::mult = val / 100; }
+
+    float SMS_cena() {
+        return humane ? price : price * (1.0 + mult);
+    }
+
+    ~SpecialSMS() { }
+};
+
+float RegularSMS::mult = 3.0;
+float SpecialSMS::mult = 1.5;
+float SMS::ddv = 0.18;
+
+void vkupno_SMS(SMS** s, int n) {
+    int reg = 0, sp = 0;
+    float reg_price = 0, sp_price = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(dynamic_cast<RegularSMS*>(s[i])) {
+            reg++;
+            reg_price += s[i]->SMS_cena();
+        } else {
+            sp++;
+            sp_price += s[i]->SMS_cena();
+        }
+    }
+
+    cout << "Vkupno ima " << reg << " regularni SMS poraki i nivnata cena e: " << reg_price << "\n";
+    cout << "Vkupno ima " << sp << " specijalni SMS poraki i nivnata cena e: " << sp_price << "\n";
+}
+
+int main(){
+
+    char tel[20], msg[1000];
+    float cena;
+    float price;
+    int p;
+    bool roam, hum;
+    SMS  **sms;
+    int n;
+    int tip;
+
+    int testCase;
+    cin >> testCase;
+
+    if (testCase == 1){
+        cout << "====== Testing RegularSMS class ======" << endl;
+        cin >> n;
+        sms = new SMS *[n];
+
+        for (int i = 0; i < n; i++){
+            cin >> tel;
+            cin >> cena;
+            cin.get();
+            cin.getline(msg, 1000);
+            cin >> roam;
+            cout << "CONSTRUCTOR" << endl;
+            sms[i] = new RegularSMS(tel, cena, msg, roam);
+            cout << "OPERATOR <<" << endl;
+            cout << *sms[i];
+        }
+        for (int i = 0; i<n; i++) delete sms[i];
+        delete[] sms;
+    }
+    if (testCase == 2){
+        cout << "====== Testing SpecialSMS class ======" << endl;
+        cin >> n;
+        sms = new SMS *[n];
+
+        for (int i = 0; i < n; i++){
+            cin >> tel;
+            cin >> cena;
+            cin >> hum;
+            cout << "CONSTRUCTOR" << endl;
+            sms[i] = new SpecialSMS(tel, cena, hum);
+            cout << "OPERATOR <<" << endl;
+            cout << *sms[i];
+        }
+        for (int i = 0; i<n; i++) delete sms[i];
+        delete[] sms;
+    }
+    if (testCase == 3){
+        cout << "====== Testing method vkupno_SMS() ======" << endl;
+        cin >> n;
+        sms = new SMS *[n];
+
+        for (int i = 0; i<n; i++){
+
+            cin >> tip;
+            cin >> tel;
+            cin >> cena;
+            if (tip == 1) {
+
+                cin.get();
+                cin.getline(msg, 1000);
+                cin >> roam;
+
+                sms[i] = new RegularSMS(tel, cena, msg, roam);
+
+            }
+            else {
+                cin >> hum;
+
+                sms[i] = new SpecialSMS(tel, cena, hum);
+            }
+        }
+
+        vkupno_SMS(sms, n);
+        for (int i = 0; i<n; i++) delete sms[i];
+        delete[] sms;
+    }
+    if (testCase == 4){
+        cout << "====== Testing RegularSMS class with a changed percentage======" << endl;
+        SMS *sms1, *sms2;
+        cin >> tel;
+        cin >> cena;
+        cin.get();
+        cin.getline(msg, 1000);
+        cin >> roam;
+        sms1 = new RegularSMS(tel, cena, msg, roam);
+        cout << *sms1;
+
+        cin >> tel;
+        cin >> cena;
+        cin.get();
+        cin.getline(msg, 1000);
+        cin >> roam;
+        cin >> p;
+        RegularSMS::set_rProcent(p);
+        sms2 = new RegularSMS(tel, cena, msg, roam);
+        cout << *sms2;
+
+        delete sms1, sms2;
+    }
+    if (testCase == 5){
+        cout << "====== Testing SpecialSMS class with a changed percentage======" << endl;
+        SMS *sms1, *sms2;
+        cin >> tel;
+        cin >> cena;
+        cin >> hum;
+        sms1 = new SpecialSMS(tel, cena, hum);
+        cout << *sms1;
+
+        cin >> tel;
+        cin >> cena;
+        cin >> hum;
+        cin >> p;
+        SpecialSMS::set_sProcent(p);
+        sms2 = new SpecialSMS(tel, cena, hum);
+        cout << *sms2;
+
+        delete sms1, sms2;
+    }
+
+    return 0;
+}
